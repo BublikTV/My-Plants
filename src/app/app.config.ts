80f812a22d206774a9663e074515eab8a75b8785
@p1398098 <s192447@student.pg.edu.pl>
2024-11-24 16:20:18 +0100

    style: enhance add-plant component with Angular Material form fields and improve layout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/app/components/add-plant/add-plant.component.ts
+++ src/app/components/add-plant/add-plant.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-plant',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './add-plant.component.html',
-  styleUrl: './add-plant.component.css'
+  styleUrls: ['./add-plant.component.css']
 })
-export class AddPlantComponent {
-
-}
+export class AddPlantComponent {}
