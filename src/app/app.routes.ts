328cda08bf511244894d9e89c75759afc914e1b5
@p1398098 <s192447@student.pg.edu.pl>
2024-11-24 19:35:48 +0100

    feat: add home and plant details components with routing and improved UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { MyPlantsComponent } from './components/my-plants/my-plants.component';
 import { AddPlantComponent } from './components/add-plant/add-plant.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlantDetailsComponent } from './components/plant-details/plant-details.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'my-plants', component: MyPlantsComponent },
   { path: 'add-plant', component: AddPlantComponent },
-  { path: '', redirectTo: 'my-plants', pathMatch: 'full' } // Domyślna strona
+  { path: 'plant-details/:id', component: PlantDetailsComponent },
+  // { path: '', redirectTo: 'my-plants', pathMatch: 'full' } // Domyślna strona
 ];
--- src/app/components/plant-details/plant-details.component.ts
+++ src/app/components/plant-details/plant-details.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Dodano CommonModule
+import { PlantService, Plant } from 'src/app/services/plant.service';
 
 @Component({
   selector: 'app-plant-details',
-  imports: [],
+  standalone: true, // Ustawione na standalone
+  imports: [CommonModule], // Import CommonModule dla dyrektywy *ngIf
   templateUrl: './plant-details.component.html',
-  styleUrl: './plant-details.component.css'
+  styleUrls: ['./plant-details.component.css']
 })
-export class PlantDetailsComponent {
+export class PlantDetailsComponent implements OnInit {
+  plant: Plant | undefined;
 
+  constructor(private route: ActivatedRoute, private plantService: PlantService) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.plantService.getPlants().subscribe((plants: Plant[]) => {
+      this.plant = plants.find(p => p.id === id);
+    });
+  }
 }
